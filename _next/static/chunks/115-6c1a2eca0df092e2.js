bb7228c692922676154c1a372f8c101da9b7aaa7
cupidbow20000 <hiroshi.tokudome0921@gmail.com>
2026-07-30 19:43:10 -0400

    sf

--- _next/static/chunks/115-6c1a2eca0df092e2.js
+++ _next/static/chunks/115-6c1a2eca0df092e2.js
@@ -268,7 +268,7 @@
                 //     size: "size-[18px]"
                 // }, 
                 {
-                    url: "https://dexscreener.com/solana/BRQMbo56SoW5Y7U6CTAqiAoCw2MPoGbsPfdqetbbpump",
+                    url: "https://dexscreener.com/solana/TBA",
                     icon: c.hfL,
                     size: "size-[18px]"
                 }],
